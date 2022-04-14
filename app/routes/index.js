6eadc05946a7ccbfdbe5d4cb2bfb4b76caa85637
Artyom Melichow <artyom.melichow@voestalpine.com>
2022-04-14 10:11:52 +0200

    undefined RouteInfo reproduction

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,14 @@ import { service } from '@ember/service';
 export default class IndexRoute extends Route {
   @service store;
 
+  beforeModel(transition) {
+    transition.to.find((routeInfo, index, array) => {
+      console.log('routeInfo: ', routeInfo);
+      console.log('index: ', index);
+      console.log('array: ', array);
+    });
+  }
+
   async model() {
     return this.store.findAll('rental');
   }
